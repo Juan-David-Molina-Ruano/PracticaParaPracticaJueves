0371bea4cc37f0050e8829852d904b4dae489222
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-20 18:20:11 -0600

    add new 2 migrations

--- PracticaParaPracticaJueves/Migrations/20240321001153_migration2032024611.Designer.cs
+++ PracticaParaPracticaJueves/Migrations/20240321001153_migration2032024611.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PracticaParaPracticaJueves.Models;
 
@@ -11,9 +12,10 @@ using PracticaParaPracticaJueves.Models;
 namespace PracticaParaPracticaJueves.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240321001153_migration2032024611")]
+    partial class migration2032024611
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PracticaParaPracticaJueves/Migrations/20240321001153_migration2032024611.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PracticaParaPracticaJueves.Migrations
+{
+    public partial class migration2032024611 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rol",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RolId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Rols",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rols", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RolId",
+                table: "Users",
+                column: "RolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Rols_RolId",
+                table: "Users",
+                column: "RolId",
+                principalTable: "Rols",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Rols_RolId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Rols");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RolId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RolId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rol",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
