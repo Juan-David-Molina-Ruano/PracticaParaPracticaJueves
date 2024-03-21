6f6b3235811414e5dc6f780f65b3b10e8913f768
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-03-20 23:11:39 -0600

    add initial create

--- PracticaParaPracticaJueves/Models/ApplicationDbContext.cs
+++ PracticaParaPracticaJueves/Models/ApplicationDbContext.cs
@@ -19,6 +19,11 @@ namespace PracticaParaPracticaJueves.Models
                 .HasMany(p => p.Phones)
                 .WithOne(d => d.Customer)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            //insertar datos inicales a la tabla rols
+            modelBuilder.Entity<Rol>().HasData(new Rol { Id = 1, Name = "Admin", Description = "soy admin" });
+
+            modelBuilder.Entity<User>().HasData(new User { Id = 1, Username = "Root", RolId = 1, Password = "e10adc3949ba59abbe56e057f20f883e" });
         }
     }
 }
